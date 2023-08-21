404cbb54a09714615c7d4c8571ab46bf1f31b004
Bahtah <btolikov@gmail.com>
2023-08-21 18:18:32 +0300

    first_commit

---
+++ ads/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Ads,Category,SubCategory
+
+
+admin.site.register(Ads)
+admin.site.register(Category)
+admin.site.register(SubCategory)
---
+++ ads/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Ads
+
+
+def ads_list(request):
+    all_ads = Ads.objects.all()
+    context = {
+        "all_ads": all_ads
+    }
+    return render(request, 'index.html', context=context)
+
