263f3b22975d241dcc8349f00afb4c065306b629
Bahtah <btolikov@gmail.com>
2023-08-25 18:16:00 +0300

    refactor code

--- ads/urls.py
+++ ads/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from .views import  category_list, create_ad, update_ad, retrieve_ad, delete_ad, AdsView
+from .views import ads_list, category_list, create_ad, update_ad, retrieve_ad, delete_ad
 
 
 urlpatterns = [
-    path("", AdsView.as_view(), name="index"),
-    # path("", ads_list, name="index"),
+    path("", ads_list, name="index"),
     path("category", category_list, name="category"),
     path("create", create_ad, name="create"),
     path("update_ad/<int:pk>/", update_ad, name="update_ad"),
--- ads/views.py
+++ ads/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib import messages
 from datetime import datetime, date, time
 from django.views import View
+from django.views.generic import  ListView
 
 from users.models import User
 
@@ -17,28 +18,22 @@ def category_list(request):
     }
     return render(request, 'category.html', context=context)
 
-# def ads_list(request):
-#     all_ads = Ads.objects.all()
-#     # all_ads = Ads.objects.filter(created_at__day__lt=24)
-#     # all_ads = Ads.objects.filter(title__icontains="test")
-#     # all_ads = Ads.objects.filter(owner__isnull=True)
-#     # all_ads = Ads.objects.filter(created_at__year=2023) & Ads.objects.filter(description__contains="b")
-#     # admin = User.objects.get(username="mentor")
-#     # all_ads = Ads.objects.filter(owner=admin)
-#     # all_ads = Ads.objects.filter(price__in=[200, 3000, 343, 2500])
-#     context = {
-#         "all_ads": all_ads
-#     }
-#     return render(request, 'index.html', context=context)
+def ads_list(request):
+    all_ads = Ads.objects.all()
+    # all_ads = Ads.objects.filter(created_at__day__lt=24)
+    # all_ads = Ads.objects.filter(title__icontains="test")
+    # all_ads = Ads.objects.filter(owner__isnull=True)
+    # all_ads = Ads.objects.filter(created_at__year=2023) & Ads.objects.filter(description__contains="b")
+    # admin = User.objects.get(username="mentor")
+    # all_ads = Ads.objects.filter(owner=admin)
+    # all_ads = Ads.objects.filter(price__in=[200, 3000, 343, 2500])
+    context = {
+        "all_ads": all_ads
+    }
+    return render(request, 'index.html', context=context)
     
-    
-class AdsView(View):
-   template_name = "index.html"
-   def get(self, request, *args, ** kwargs):
-       all_ads = Ads.objects.all()
-       return render(request, self.template_name, {"all_ads": all_ads})
-        
-    
+            
+
 def create_ad(request):
     if request.method == "POST":
         form = AdForm(request.POST, request.FILES)
